926e8175a21c5dd63ad0a144831520f5289fc6f5
Ahmad syr <Ahmad.sarji1999@gmail.com>
2023-09-22 14:54:37 +0300

    App initialization

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApplication());
+}
+class MyApplication extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+    );
+  }
+
+}
+
+
