3cbbcd36329d4bcd587d32facfa913439f562deb
Ahmad syr <Ahmad.sarji1999@gmail.com>
2023-09-24 01:44:40 +0300

    displayed quraan text and did some edits to the design

--- lib/modules/Quraan/quranTextView.dart
+++ lib/modules/Quraan/quranTextView.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/modules/Quraan/quraanWidget.dart';
 
-class QuranTextView extends StatelessWidget {
-  const QuranTextView({super.key});
+class QuranTextView extends StatefulWidget {
+  QuranTextView({super.key});
 
   static const String routeName = 'SuraText';
 
   @override
+  State<QuranTextView> createState() => _QuranTextViewState();
+}
+
+class _QuranTextViewState extends State<QuranTextView> {
+  String surahContent = '';
+  List<String> allContent = [];
+
+  @override
   Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as SuraDetails;
+    if (surahContent.isEmpty) {
+      loadFiles(args.suraNumber);
+    }
     var theme = Theme.of(context);
     var mediaQuery = MediaQuery.of(context).size;
+
     return Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -35,7 +50,7 @@ class QuranTextView extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Sura Name',
+                      args.suraName,
                       style: theme.textTheme.bodyLarge,
                     ),
                     SizedBox(width: 20),
@@ -52,13 +67,31 @@ class QuranTextView extends StatelessWidget {
                   thickness: 1,
                   height: 1,
                 ),
-                Text(
-                  '',
-                  style: theme.textTheme.bodySmall,
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (
+                      BuildContext context,
+                      int index,
+                    ) {
+                      return Text('(${index + 1})${allContent[index]}',
+                          style: theme.textTheme.bodySmall,
+                          textAlign: TextAlign.center);
+                    },
+                    itemCount: allContent.length,
+                  ),
                 )
               ],
             ),
           ),
         ));
+  }
+
+  loadFiles(String index) async {
+    String surah = await rootBundle.loadString("assets/files/$index.txt");
+    surahContent = surah;
+    List<String> Lines = surahContent.split('\n');
+    print(Lines);
+    allContent = Lines;
+    setState(() {});
   }
 }
